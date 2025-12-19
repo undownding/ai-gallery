be0462c6c0b3601d4899ad718973985d0b3f6ed6
undownding <undownding@gmail.com>
2025-12-19 21:13:34 +0800

    update migration

--- drizzle/0000_small_lily_hollister.sql
+++ drizzle/0000_small_lily_hollister.sql
@@ -1,3 +1,10 @@
+CREATE TABLE `uploads` (
+	`id` text PRIMARY KEY NOT NULL,
+	`key` text NOT NULL,
+	`etag` text NOT NULL,
+	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE `articles` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text NOT NULL,
@@ -5,21 +12,9 @@ CREATE TABLE `articles` (
 	`is_public` integer DEFAULT false NOT NULL,
 	`preview_image_id` text,
 	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
+	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	FOREIGN KEY (`preview_image_id`) REFERENCES `uploads`(`id`) ON UPDATE cascade ON DELETE set null
 );
---> statement-breakpoint
-CREATE TABLE `uploads` (
-	`id` text PRIMARY KEY NOT NULL,
-	`key` text NOT NULL,
-	`etag` text NOT NULL,
-	`article_id` text,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE `articles`
-ADD CONSTRAINT `articles_preview_image_id_uploads_id_fk`
-FOREIGN KEY (`preview_image_id`) REFERENCES `uploads`(`id`) ON UPDATE cascade ON DELETE set null;
 --> statement-breakpoint
 ALTER TABLE `uploads`
-ADD CONSTRAINT `uploads_article_id_articles_id_fk`
-FOREIGN KEY (`article_id`) REFERENCES `articles`(`id`) ON UPDATE cascade ON DELETE set null;
+ADD COLUMN `article_id` text REFERENCES `articles`(`id`) ON UPDATE cascade ON DELETE set null;
